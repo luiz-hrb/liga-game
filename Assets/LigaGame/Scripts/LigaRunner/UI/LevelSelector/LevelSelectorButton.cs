78b5812f37375d2d5d7b06f4a8e205e1e6444498
Luiz Henrique Barbosa <luiz.hrb2@gmail.com>
2021-11-03 19:22:19 -0300

    Adding time on button of level selector

--- Assets/LigaGame/Scripts/LigaRunner/UI/LevelSelector/LevelSelectorButton.cs
+++ Assets/LigaGame/Scripts/LigaRunner/UI/LevelSelector/LevelSelectorButton.cs
@@ -3,12 +3,14 @@ using UnityEngine;
 using UnityEngine.UI;
 using TMPro;
 using LigaGame.ScriptableObjects;
+using LigaGame.ExtensionMethods;
 
 namespace LigaGame.UI.Menu.LevelSelector
 {
     public class LevelSelectorButton : MonoBehaviour
     {
         [SerializeField] private TextMeshProUGUI _nameText;
+        [SerializeField] private TextMeshProUGUI _timeText;
         [SerializeField] private Image _iconImage;
         [SerializeField] private Button _button;
         [SerializeField] private ScoreView _scoreMarks;
@@ -19,6 +21,7 @@ namespace LigaGame.UI.Menu.LevelSelector
         public void Initialize(LevelData levelData, LevelProgressData levelProgressData, Action onClickAction = null)
         {
             _nameText.text = levelData.Name;
+            _timeText.text = levelProgressData.GameplayTime.FromSecondsToTime();
             _iconImage.sprite = levelData.Icon;
             _scoreMarks.SetScoreItensQuantity(levelData.QuantityStars);
             MarkProgress(levelProgressData);
